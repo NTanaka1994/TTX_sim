bf0d1f3662cd0a925989dad85f2876d39ed9e8ba
N Tanaka <66735232+NTanaka1994@users.noreply.github.com>
2025-12-03 15:45:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -20,10 +20,22 @@ def route():
 
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home_ver2.html")
 
-@app.route("/discussion/<incident_num>/<role>")
-def discussion(incident_num, role):
+@app.route("/discussion/<incident_num>/<name>")
+def discussion(incident_num, name):
+    con = sqlite3.connect("TTX.db")
+    cur = con.cursor()
+    cur.execute("SELECT user FROM chat WHERE id=?",(incident_num,))
+    tmp = []
+    for row in cur:
+        tmp.append(row[0])
+    print(len(tmp))
+    if "role" not in session:
+        if len(tmp) == 0:
+            session["role"] = "manager"
+        else:
+            session["role"] = "player " + name
     now = datetime.now()
     now_str = now.strftime('%Y-%m-%d %H:%M:%S')
     if incident_num not in session:
@@ -31,7 +43,10 @@ def discussion(incident_num, role):
     res = ""
     con = sqlite3.connect("TTX.db")
     cur = con.cursor()
-    cur.execute("SELECT user, comment FROM chat WHERE id=? AND time>?",(incident_num,session[incident_num]))
+    if session["role"] == "manager":
+        cur.execute("SELECT user, comment FROM chat WHERE id=? AND time>?",(incident_num,session[incident_num]))
+    else:
+        cur.execute("SELECT user, comment FROM chat WHERE id=? AND time>? AND user!=?",(incident_num,session[incident_num], "redteam"))
     for user, text in cur:
         res += html.escape(user) + ": " + html.escape(text) + "<br>"
     con.close()
@@ -43,71 +58,188 @@ def discussion(incident_num, role):
         jsn = row[0]
     dic = json.loads(jsn)
     con.close()
-    return render_template("discussion.html", incident_num=incident_num, incident=html.escape(dic["name"]), des=html.escape(dic["description"]), role=html.escape(role), chat=res)
+    if session["role"] == "manager":
+        return render_template("discussion_manager.html", incident_num=incident_num, incident=html.escape(dic["name"]), des=html.escape(dic["description"]), role=html.escape(session["role"]), chat=res, name=name)
+    else:
+        return render_template("discussion_player.html", incident_num=incident_num, incident=html.escape(dic["name"]), des=html.escape(dic["description"]), role=html.escape(session["role"]), chat=res)    
 
 @app.route("/judge/<incident_num>", methods=["POST"])
 def judge(incident_num):
-    result = request.form["result"]
-    chat = ""
-    con = sqlite3.connect("TTX.db")
-    cur = con.cursor()
-    cur.execute("SELECT time, user, comment FROM chat WHERE id=?",(incident_num,))
-    chat += "会話記録\n"
-    for time, user, comment in cur:
-        chat += "時刻 : " + html.escape(str(time)) + "\t" +html.escape(user) + " \t: " + html.escape(comment) + "\n"
-    res = chat + "\n" + "対応 : " + html.escape(result)
-    con.close()
-    con = sqlite3.connect("TTX.db")
-    jsn = ""
-    cur = con.cursor()
-    cur.execute("SELECT json FROM incident WHERE id=?", (incident_num,))
-    for row in cur:
-        jsn = row[0]
-    dic2 = json.loads(jsn)
-    con.close()
-    client = OpenAI(api_key=key)
-    prompt = f"""
-    あなたはサイバー攻撃対処の専門家です。
-    次のインシデント対応とその過程の議事録について100点満点で採点してください。
-    
-    # インシデント内容
-    {dic2['name']}
-    
-    # 採点基準
-    1. 技術的妥当性(40点)
-    2. 組織的対応と迅速性(30点)
-    3. 被害抑止への貢献度(30点)
-    
-    # チーム内での会話議事録
-    {chat}
-    
-    # 最終回答
-    {result}
-    
-    出力形式は次のJSONとしてください。
-    {{
-        "score" : 数値,
-        "reason" : "理由の説明",
-        "advice" : "改善点"
-    }}
-    """
-    response = client.chat.completions.create(model="gpt-4o-mini", messages=[{"role":"user","content":prompt}])
-    mes = response.choices[0].message.content
-    try:
-        dic = json.loads(mes)
-    except:
-        mes = mes.replace("```json", "")
-        mes = mes.replace("\n```", "")
-        print(mes)
-        dic = json.loads(mes)
+    if "num" not in session:
+        session["num"] = 1
+        name = request.form["name"]
+        result = request.form["result"]
+        chat = ""
+        con = sqlite3.connect("TTX.db")
+        cur = con.cursor()
+        cur.execute("SELECT time, user, comment FROM chat WHERE id=?",(incident_num,))
+        chat += "会話記録\n"
+        for time, user, comment in cur:
+            chat += "時刻 : " + html.escape(str(time)) + "\t" +html.escape(user) + " \t: " + html.escape(comment) + "\n"
+        res = chat + "\n" + "対応 : " + html.escape(result)
+        con.close()
+        con = sqlite3.connect("TTX.db")
+        jsn = ""
+        cur = con.cursor()
+        cur.execute("SELECT json FROM incident WHERE id=?", (incident_num,))
+        for row in cur:
+            jsn = row[0]
+        dic2 = json.loads(jsn)
+        con.close()
+        client = OpenAI(api_key=key)
+        prompt = f"""
+        あなたはサイバー攻撃の専門家です。
+        次のインシデント対応とその過程から次の攻撃内容を記載してください。
         
-    res = "<h2>点数</h2>" + str(dic["score"]) + "点\n<h2>理由</h2>" + html.escape(dic["reason"]).replace("。", "。\n") + "\n<h2>アドバイス</h2>\n" + html.escape(dic["advice"]).replace("。", "。\n") + "\n以下議事録と最終決定" + res
-    con = sqlite3.connect("TTX.db")
-    cur = con.cursor()
-    cur.execute("DELETE FROM chat WHERE id=?",(incident_num,))
-    con.commit()
-    con.close()
-    return render_template("judge.html", res=res, incident=html.escape(dic2["name"]), des=html.escape(dic2["description"]))
+        # インシデント内容
+        {dic2['name']}
+                
+        # チーム内での会話議事録
+        {chat}
+        
+        # 対応
+        {result}
+        
+        出力形式は次のJSONとしてください。
+        {{
+            "next" : "次の攻撃内容",
+        }}
+        """
+        response = client.chat.completions.create(model="gpt-4o-mini", messages=[{"role":"user","content":prompt}])
+        mes = response.choices[0].message.content
+        try:
+            dic = json.loads(mes)
+        except:
+            mes = mes.replace("```json", "")
+            mes = mes.replace("\n```", "")
+            print(mes)
+            dic = json.loads(mes)
+        now = datetime.now()
+        now_str = now.strftime('%Y-%m-%d %H:%M:%S')
+        con = sqlite3.connect("TTX.db")
+        cur = con.cursor()
+        cur.execute("INSERT INTO chat (id, user, comment, time) VALUES (?, ?, ?, ?)", (incident_num, "redteam", dic["next"], now_str))
+        con.commit()
+        con.close()
+        return redirect("../../discussion/"+ incident_num + "/" + name)
+    if session["num"] < 3:
+        session["num"] += 1
+        name = request.form["name"]
+        result = request.form["result"]
+        chat = ""
+        con = sqlite3.connect("TTX.db")
+        cur = con.cursor()
+        cur.execute("SELECT time, user, comment FROM chat WHERE id=?",(incident_num,))
+        chat += "会話記録\n"
+        for time, user, comment in cur:
+            chat += "時刻 : " + html.escape(str(time)) + "\t" +html.escape(user) + " \t: " + html.escape(comment) + "\n"
+        res = chat + "\n" + "対応 : " + html.escape(result)
+        con.close()
+        con = sqlite3.connect("TTX.db")
+        jsn = ""
+        cur = con.cursor()
+        cur.execute("SELECT json FROM incident WHERE id=?", (incident_num,))
+        for row in cur:
+            jsn = row[0]
+        dic2 = json.loads(jsn)
+        con.close()
+        client = OpenAI(api_key=key)
+        prompt = f"""
+        あなたはサイバー攻撃対処の専門家です。
+        次のインシデント対応とその過程から次の攻撃内容を記載してください。
+        
+        # インシデント内容
+        {dic2['name']}
+                
+        # チーム内での会話議事録
+        {chat}
+        
+        # 対応
+        {result}
+        
+        出力形式は次のJSONとしてください。
+        {{
+            "next" : "次の攻撃内容",
+        }}
+        """
+        response = client.chat.completions.create(model="gpt-4o-mini", messages=[{"role":"user","content":prompt}])
+        mes = response.choices[0].message.content
+        try:
+            dic = json.loads(mes)
+        except:
+            mes = mes.replace("```json", "")
+            mes = mes.replace("\n```", "")
+            print(mes)
+            dic = json.loads(mes)
+        now = datetime.now()
+        now_str = now.strftime('%Y-%m-%d %H:%M:%S')
+        con = sqlite3.connect("TTX.db")
+        cur = con.cursor()
+        cur.execute("INSERT INTO chat (id, user, comment, time) VALUES (?, ?, ?, ?)", (incident_num, "redteam", dic["next"], now_str))
+        con.commit()
+        con.close()
+        return redirect("../../discussion/"+incident_num+"/"+name)
+    elif session["num"] == 3:
+        result = request.form["result"]
+        chat = ""
+        con = sqlite3.connect("TTX.db")
+        cur = con.cursor()
+        cur.execute("SELECT time, user, comment FROM chat WHERE id=?",(incident_num,))
+        chat += "会話記録\n"
+        for time, user, comment in cur:
+            chat += "時刻 : " + html.escape(str(time)) + "\t" +html.escape(user) + " \t: " + html.escape(comment) + "\n"
+        res = chat + "\n" + "対応 : " + html.escape(result)
+        con.close()
+        con = sqlite3.connect("TTX.db")
+        jsn = ""
+        cur = con.cursor()
+        cur.execute("SELECT json FROM incident WHERE id=?", (incident_num,))
+        for row in cur:
+            jsn = row[0]
+        dic2 = json.loads(jsn)
+        con.close()
+        client = OpenAI(api_key=key)
+        prompt = f"""
+        あなたはサイバー攻撃対処の専門家です。
+        次のインシデント対応とその過程の議事録について100点満点で採点してください。
+        
+        # インシデント内容
+        {dic2['name']}
+        
+        # 採点基準
+        1. 技術的妥当性(40点)
+        2. 組織的対応と迅速性(30点)
+        3. 被害抑止への貢献度(30点)
+        
+        # チーム内での会話議事録
+        {chat}
+        
+        # 最終回答
+        {result}
+        
+        出力形式は次のJSONとしてください。
+        {{
+            "score" : 数値,
+            "reason" : "理由の説明",
+            "advice" : "改善点"
+        }}
+        """
+        response = client.chat.completions.create(model="gpt-4o-mini", messages=[{"role":"user","content":prompt}])
+        mes = response.choices[0].message.content
+        try:
+            dic = json.loads(mes)
+        except:
+            mes = mes.replace("```json", "")
+            mes = mes.replace("\n```", "")
+            print(mes)
+            dic = json.loads(mes)  
+        res = "<h2>点数</h2>" + str(dic["score"]) + "点\n<h2>理由</h2>" + html.escape(dic["reason"]).replace("。", "。\n") + "\n<h2>アドバイス</h2>\n" + html.escape(dic["advice"]).replace("。", "。\n") + "\n以下議事録と最終決定" + res
+        con = sqlite3.connect("TTX.db")
+        cur = con.cursor()
+        cur.execute("DELETE FROM chat WHERE id=?",(incident_num,))
+        con.commit()
+        con.close()
+        return render_template("judge.html", res=res, incident=html.escape(dic2["name"]), des=html.escape(dic2["description"]))
 
 @app.route("/logout")
 def logout():
@@ -118,6 +250,13 @@ def logout():
 def on_join(data):
     room = data["room"]
     join_room(room)
+    now = datetime.now()
+    now_str = now.strftime('%Y-%m-%d %H:%M:%S')
+    con = sqlite3.connect("TTX.db")
+    cur = con.cursor()
+    cur.execute("INSERT INTO chat (id, user, comment, time) values (?, ?, ?, ?)",(data["room"], data["user"], "join", now_str))
+    con.commit()
+    con.close()
     emit("chat", {"msg" : f"{ data['user']} joined" }, room=room)
 
 @socketio.on("message")
